a22bdd4e9eb1df3fe3ff3fbcb06a2c1e1a21e03d
Sanjay Rajasekaran <161409791+Sanjayr181@users.noreply.github.com>
2024-03-27 15:57:54 +0530

    Add files via upload

---
+++ five.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Program to run my other four programs
+# Run the questionnaire.sh file
+./questionnaire.sh
+# Run the countdown.sh file with argument 3
+./countdown.sh 3
+
+# Run the bingo.sh file
+./bingo.sh
+
+# Run the fortune.sh file
+./fortune.sh
